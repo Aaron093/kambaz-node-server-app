bbd29938cc26a55ac706076913b455f6874f6aa0
Aaron093 <97973556+Aaron093@users.noreply.github.com>
2025-04-02 23:59:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ import EnrollmentRoutes from './Kambaz/Enrollments/routes.js';
 const app = express()
 app.use(cors({
   credentials: true,
-  // origin: process.env.NETLIFY_URL || "http://localhost:5173" || "https://yiqianzhang-assignment5.netlify.app/",
-  origin: true,
+  origin: process.env.NETLIFY_URL || "http://localhost:5173",
 }
 )); 
 
